e637ee31d8831ff5eea2ebf3a1873742c80f651f
eliasborges <eliasborges@unipam.edu.br>
2021-09-15 19:15:50 -0300

    Ajuste metodo updatePassWord

--- src/main/java/com/api/controlfood/controller/dto/request/UserUpdateRequest.java
+++ src/main/java/com/api/controlfood/controller/dto/request/UserUpdateRequest.java
@@ -1,9 +1,6 @@
 package com.api.controlfood.controller.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -11,6 +8,7 @@ import javax.validation.constraints.Size;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
 @ToString
 public class UserUpdateRequest {
